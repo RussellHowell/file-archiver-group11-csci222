4413dc21657e801f38f3a8d521ad5df4af731932
dpekkle <danielle.cerisier@gmail.com>
2015-09-09 18:25:24 +1000

    Basic Structure for versionrec

--- VersionRec.h
+++ VersionRec.h
@@ -1,13 +1,62 @@
-/* 
- * File:   VersionRec.h
- * Author: russell
- *
- * Created on 3 September 2015, 12:10 AM
- */
+// *
+// *
+// * CSCI222
+// * Group 11
+// * 
+// * Author(s): Danielle Cerisier 
+// * Last modified: September 9th, 2015
+// * Description:
+// * Purpose:
+#include <QtCore/QCoreApplication>
+#include <QApplication>
+#include <QFile>
+#include <QDebug> 
 
 #ifndef VERSIONREC_H
 #define	VERSIONREC_H
 
+ struct timestruc
+ {
+        //timespec structure, as in FileRec.h
+ };
+
+class VersionRec
+{
+    
+private:
+    QString version_ID; //generated primary key or "GENOID" in notes
+    qint16 version_num;
+    qint64 length; //number of elements in record_collections
+    timestruc modify_time;
+  
+    
+    // hash member
+    
+    // STL collection of blocks
+        // struct block
+            // qint64 block_num
+            // block hash
+            // qint64 block_length
+            // byte array (compressed data for the file block)
+    
+public:
+    VersionRec();
+    Qstring getVersionID();
+    void setVersionID(Qstring);
+    qint16 getVersionNum();
+    void setVersionNum(qint16);
+    qint64 getLength();
+    void setLength(qint64);
+    modify_time getModifyTime();
+    void setModifyTime(timestruc);
+    // getHash
+    // setHash
+    // getDiff
+    // setDiff
+    
+    void transferToDB();
+    void transferFromDB();
+};
 
 
 #endif	/* VERSIONREC_H */
